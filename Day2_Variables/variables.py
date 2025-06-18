e8114f5878bacb56680405f1657a687003480a45
Abiy A <abiyayele0@gmail.com>
2025-06-18 17:08:00 +0200

    Day 2: Variables and Data Types + Profile Card

---
+++ Day2_Variables/variables.py
@@ -0,0 +1,26 @@
+# Day 2: Variables & Data Types
+
+
+# string (text) 
+name = "Abiy"
+
+
+# Intiger (whole number)
+age = 30
+
+# Float (decimal number)
+height = 1.75
+
+
+# Boolean (True or False)
+is_student = True
+
+
+# Print them
+print("Name:",name)
+print("Age:",age)
+print("Hight:",height)
+print("learning python:",is_student)
+
+
+
